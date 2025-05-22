770e5c6589b91dc5f405287c023a45cf6ac10e7d
scristiansc <scristian1012@gmail.com>
2025-05-22 13:08:21 -0500

    completed create and index for hotels

--- app/controllers/hotels_controller.rb
+++ app/controllers/hotels_controller.rb
@@ -1,4 +1,24 @@
 class HotelsController < ApplicationController
-    def new
+  def new
+  end
+
+  def index
+    @hotels = Hotel.all
+  end
+
+  def create
+    hotel = Hotel.create(
+      name: params[:hotel][:name],
+      address: params[:hotel][:address],
+      rating: params[:hotel][:rating]
+    )
+
+    if hotel.persisted?
+      flash.notice = "Hotel created successfully"
+    else
+      flash.alert = "Something went wrong"
     end
+
+    redirect_to hotels_path
+  end
 end
