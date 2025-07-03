42fcfec8f32addc0a2caaab1c0f51276918e6ecd
scristiansc <scristian1012@gmail.com>
2025-07-03 12:12:17 -0500

    Fix hotel null column migration

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_03_050623) do
   enable_extension "pg_catalog.plpgsql"
 
   create_table "hotels", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.integer "rating"
+    t.string "name", null: false
+    t.string "address", null: false
+    t.integer "rating", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
